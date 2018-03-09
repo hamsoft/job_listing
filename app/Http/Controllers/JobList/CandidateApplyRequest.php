81d2e8c085355f1acbbc4456b48079f23daf67d3
Dusan Vlahovic <dusanvl87@gmail.com>
2018-03-09 03:22:48 +0100

    Add rules unique email per job position, mime types for CV.

--- app/Http/Controllers/JobList/CandidateApplyRequest.php
+++ app/Http/Controllers/JobList/CandidateApplyRequest.php
@@ -4,6 +4,7 @@
 namespace JobListing\Http\Controllers\JobList;
 
 
+use Illuminate\Validation\Rule;
 use JobListing\Http\Controllers\Request;
 use JobListing\Services\CandidateManager\NewCandidateData;
 
@@ -17,4 +18,16 @@ class CandidateApplyRequest extends Request implements NewCandidateData
     public function getCv() {
         return $this->file('fileCv');
     }
+
+    public function rules() {
+        $rules = parent::rules();
+        $rules['email'] = Rule::unique('candidates')->where(function ($q) {
+            $position = $this->route('position');
+            $q->where('email', '=', $this->get('email'));
+            $q->where('job_position_id', $position->getId());
+        });
+        $rules['fileCv'] = 'mimes:pdf,txt,docx,doc';
+        return $rules;
+    }
+
 }
